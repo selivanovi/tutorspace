c7e5be3d55da17517c0e0258e0ae722264df5a45
Illia Selivanov <7selivanovilya@gmail.com>
2026-01-16 13:09:42 +0300

    some changes

--- app/src/main/java/dev/selivanovi/tutorspace/MainActivity.kt
+++ app/src/main/java/dev/selivanovi/tutorspace/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TutorspaceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Mobile",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
